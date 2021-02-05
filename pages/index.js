ebf4a9557faf264fe30fa8141afd0571d385a818
Buddy Reno <thebudman84@gmail.com>
2021-02-05 14:03:40 -0600

    Convert homepage to Tailwind.

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,24 @@
 import Link from 'next/link'
 import PageTitle from '../components/page-title';
 import Layout from '../components/layout'
-import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
     <>
       <PageTitle />
       <Layout noNav noFooter noSpacing>
-        <div className={styles.centering}>
+        <div className="px-6 py-0 flex items-center justify-center h-screen">
           <div className="content">
-            <h1 className={styles.title}>Hi, I'm Buddy&nbsp;Reno<b className={styles.fancy}>.</b></h1>
-            <p className={styles.description}>A web developer making things in Nashville,&nbsp;TN<b className={styles.fancy}>.</b></p>
-            <nav className={styles.nav}>
-              <ul>
-                <li>
+            <h1 className="text-5xl sm:text-7xl">Hi, I'm Buddy&nbsp;Reno<b className="text-purple">.</b></h1>
+            <p className="text-3xl mb-9 sm:text-5xl">A web developer making things in Nashville,&nbsp;TN<b className="text-purple">.</b></p>
+            <nav>
+              <ul className="list-none m-0 p-0">
+                <li className="inline-block text-xl sm:text-3xl">
                   <Link href="/posts">
                     <a>Posts</a>
                   </Link>
                 </li>
-                <li>
+                <li className="inline-block ml-6 text-xl sm:text-3xl">
                   <Link href="/about">
                     <a>About</a>
                   </Link>
