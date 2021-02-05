4d310a811d16051c7b2fe4d06d23e09e5799d278
Buddy Reno <thebudman84@gmail.com>
2021-02-05 10:25:28 -0600

    Add open graph tags and meta slugs (#2)

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,34 @@
 import Link from 'next/link'
+import PageTitle from '../components/page-title';
 import Layout from '../components/layout'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <Layout noNav noFooter noSpacing>
-      <div className={styles.centering}>
-        <div className="content">
-          <h1 className={styles.title}>Hi, I'm Buddy&nbsp;Reno<b className={styles.fancy}>.</b></h1>
-          <p className={styles.description}>A web developer making things in Nashville,&nbsp;TN<b className={styles.fancy}>.</b></p>
-          <nav className={styles.nav}>
-            <ul>
-              <li>
-                <Link href="/posts">
-                  <a>Posts</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/about">
-                  <a>About</a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
+    <>
+      <PageTitle />
+      <Layout noNav noFooter noSpacing>
+        <div className={styles.centering}>
+          <div className="content">
+            <h1 className={styles.title}>Hi, I'm Buddy&nbsp;Reno<b className={styles.fancy}>.</b></h1>
+            <p className={styles.description}>A web developer making things in Nashville,&nbsp;TN<b className={styles.fancy}>.</b></p>
+            <nav className={styles.nav}>
+              <ul>
+                <li>
+                  <Link href="/posts">
+                    <a>Posts</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/about">
+                    <a>About</a>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link';
+import PageTitle from '../components/page-title';
 import Layout from '../components/layout'
 import PostExcerpt from '../components/post-excerpt';
 import { posts } from '../lib/posts';
@@ -7,13 +8,16 @@ import styles from '../styles/posts.module.scss';
 
 export default function Posts() {
   return (
-    <Layout>
-      <section className={styles.articles}>
-        <h1 className={styles.title}>Posts</h1>
-        {posts.map((post) => (
-          <PostExcerpt key={post.slug} post={post} />
-        ))}
-      </section>
-    </Layout>
+    <>
+      <PageTitle/>
+      <Layout>
+        <section className={styles.articles}>
+          <h1 className={styles.title}>Posts</h1>
+          {posts.map((post) => (
+            <PostExcerpt key={post.slug} post={post} />
+          ))}
+        </section>
+      </Layout>
+    </>
   )
 }
