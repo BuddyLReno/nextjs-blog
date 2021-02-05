17163d645bf1febfc39aba94def159de7ea061ec
Buddy Reno <buddy.reno@daveramsey.com>
2021-02-05 14:00:47 -0600

    finish homepage conversion

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import PageTitle from '../components/page-title';
 import Layout from '../components/layout'
-import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -12,14 +11,14 @@ export default function Home() {
           <div className="content">
             <h1 className="text-5xl sm:text-7xl">Hi, I'm Buddy&nbsp;Reno<b className="text-purple">.</b></h1>
             <p className="text-3xl mb-9 sm:text-5xl">A web developer making things in Nashville,&nbsp;TN<b className="text-purple">.</b></p>
-            <nav className={styles.nav}>
-              <ul>
-                <li>
+            <nav>
+              <ul className="list-none m-0 p-0">
+                <li className="inline-block text-xl sm:text-3xl">
                   <Link href="/posts">
                     <a>Posts</a>
                   </Link>
                 </li>
-                <li>
+                <li className="inline-block ml-6 text-xl sm:text-3xl">
                   <Link href="/about">
                     <a>About</a>
                   </Link>
