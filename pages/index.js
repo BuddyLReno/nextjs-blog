0a11c671869f5c02ae186bff3ca08231e0267bc4
Buddy Reno <buddy.reno@daveramsey.com>
2021-02-05 13:51:31 -0600

    Begin converting homepage to tailwind

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
     <>
       <PageTitle />
       <Layout noNav noFooter noSpacing>
-        <div className={styles.centering}>
+        <div className="px-6 py-0 flex items-center justify-center h-screen">
           <div className="content">
-            <h1 className={styles.title}>Hi, I'm Buddy&nbsp;Reno<b className={styles.fancy}>.</b></h1>
-            <p className={styles.description}>A web developer making things in Nashville,&nbsp;TN<b className={styles.fancy}>.</b></p>
+            <h1 className="text-5xl sm:text-7xl">Hi, I'm Buddy&nbsp;Reno<b className="text-purple">.</b></h1>
+            <p className="text-3xl mb-9 sm:text-5xl">A web developer making things in Nashville,&nbsp;TN<b className="text-purple">.</b></p>
             <nav className={styles.nav}>
               <ul>
                 <li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', ],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      purple: {
+        DEFAULT: '#753AD8'
+      }
+    },
     extend: {},
   },
   variants: {
