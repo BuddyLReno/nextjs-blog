4a97aa63fe1bd2e6bc9a745623e8afefa3df62fa
Buddy Reno <thebudman84@gmail.com>
2022-11-18 14:45:46 -0600

    Forgot to actually add google analytics lol

--- components/page-title.js
+++ components/page-title.js
@@ -9,7 +9,18 @@ export default function PageTitle({ title = '' }) {
     <>
       <Head>
         <title key="page-title">{format(title)}</title>
-        
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-10JBF1Y2VK"></script>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-10JBF1Y2VK', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}></script>
       </Head>
     </>
   );
